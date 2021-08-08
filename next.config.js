a4f81cc8a6c67a200455354ab285acfd8c025211
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-08-08 14:17:11 +0900

    edited next.config.js

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    MAPBOX_KEY:
+      'pk.eyJ1Ijoia3lveWEiLCJhIjoiY2tvZGpmZWE0MDJmbjJ2czNycTFzODFybSJ9.jZfW8xFNiTVhRLapT4lzng',
+  },
 }
